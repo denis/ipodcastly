fa9c848f21a7e0bc4ef8ee6d86e3434fc75a109f
Denis Barushev <barushev@gmail.com>
2010-01-10 23:23:52 +0200

    Fixed spec.

--- spec/ipodcastly/server/episode_spec.rb
+++ spec/ipodcastly/server/episode_spec.rb
@@ -1,10 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Ipodcastly::Server::Episode do
-  describe "#position" do
-    it "shows position in seconds" do
-      episode = Ipodcastly::Server::Episode.new(1, "Episode 1", 417120)
-      episode.position.should == 417.12
-    end
-  end
 end
